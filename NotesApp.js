b716e1cd558cd76bd1025171065baee6e9f878da
Mitchell Hall <mitch@aktech.com.au>
2013-07-15 23:40:10 +1000

    database working

---
+++ NotesApp.js
@@ -0,0 +1,68 @@
+var NotesApp = (function() {
+    var pub = {};
+
+    pub.start = function() {
+    // Wait for PhoneGap to load
+    //
+    onDeviceReady();
+    //document.addEventListener("deviceready", onDeviceReady, false);
+    }
+
+    // Populate the database 
+    //
+    function populateDB(tx) {
+        tx.executeSql('DROP TABLE IF EXISTS Notes');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS Notes (id AUTO_INCREMENT PRIMARY KEY, text, date_created, date_modified)');
+
+        //var formattedDate = new Date().dateFormat('mmm dd, yyyy h:mm TT');
+        var sysDate = new Date();
+
+        tx.executeSql('INSERT INTO Notes (text, date_created) VALUES ("yolo", "' + sysDate + '")');
+        tx.executeSql('INSERT INTO Notes (text, date_created) VALUES ("swag", "' + sysDate + '")');
+    }
+
+    // Query the database
+    //
+    function queryDB(tx) {
+        tx.executeSql('SELECT * FROM Notes', [], querySuccess, errorCB);
+    }
+
+    // Query the success callback
+    //
+    function querySuccess(tx, results) {
+        // this will be empty since no rows were inserted.
+        //console.log("Insert ID = " + results.insertId);
+        // this will be 0 since it is a select statement
+        console.log("Rows Affected = " + results.rowAffected);
+        // the number of rows returned by the select statement
+        console.log("Insert ID = " + results.rows.length);
+
+        var len = results.rows.length;
+
+        for (var i = 0; i < len; i++)
+            console.log("Row " + (i + 1) + ": " + results.rows.item(i).date_created);
+
+    }
+
+    // Transaction error callback
+    //
+    function errorCB(err) {
+        console.log("Error processing SQL: " + err.code);
+    }
+
+    // Transaction success callback
+    //
+    function successCB() {
+        var db = window.openDatabase("NotesDB", "1.0", "Notes App", 2 * 1000 * 1000);
+        db.transaction(queryDB, errorCB);
+    }
+
+    // PhoneGap is ready
+    //
+    function onDeviceReady() {
+        var db = window.openDatabase("NotesDB", "1.0", "Notes App", 2 * 1000 * 1000);
+        db.transaction(populateDB, errorCB, successCB);
+    }
+        
+return pub;
+}());
